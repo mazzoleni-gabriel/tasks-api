631e917c2aea1706f59d24265b53f33ff016a486
Gabriel Mazzoleni Pinto <gabriel.pinto@mercadolivre.com>
2022-09-11 13:53:12 -0300

    Publish task event feed on create use case

--- src/tasks/usecases/createtask/usecase_test.go
+++ src/tasks/usecases/createtask/usecase_test.go
@@ -13,17 +13,24 @@ import (
 func Test_UseCase(t *testing.T) {
 	t.Run("Should return created id", func(t *testing.T) {
 		entity := tasks.Task{}
+		createdID := uint(1)
 
 		writerMock := mocks.NewWriter(t)
 		writerMock.On("Create", mock.Anything, entity).
-			Return(uint(1), nil)
+			Return(createdID, nil)
 
-		useCase := createtask.NewUseCase(writerMock)
+		publishedTask := tasks.Task{ID: createdID}
+		publisherMock := mocks.NewPublisher(t)
+		publisherMock.On("PublishCreateMessage", mock.Anything, publishedTask).
+			Return(nil)
+
+		useCase := createtask.NewUseCase(writerMock, publisherMock)
 		createdID, err := useCase.Create(context.TODO(), entity)
 
 		assert.NoError(t, err)
-		assert.Equal(t, uint(1), createdID)
+		assert.Equal(t, createdID, createdID)
 		writerMock.AssertExpectations(t)
+		publisherMock.AssertExpectations(t)
 	})
 
 	t.Run("Should return error when writer fails", func(t *testing.T) {
@@ -33,7 +40,7 @@ func Test_UseCase(t *testing.T) {
 		writerMock.On("Create", mock.Anything, entity).
 			Return(uint(0), assert.AnError)
 
-		useCase := createtask.NewUseCase(writerMock)
+		useCase := createtask.NewUseCase(writerMock, nil)
 		_, err := useCase.Create(context.TODO(), entity)
 
 		assert.Error(t, err)
